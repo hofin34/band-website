e446b5d9d2161336e90fd34f179e0d15da09f0b2
Martin Hofbauer <m.hofbauer@email.cz>
2023-07-25 19:45:28 +0200

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,35 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Flex, HStack, Heading, IconButton, Spacer, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
+import { BsFillTelephoneFill, BsFacebook, BsInstagram } from 'react-icons/bs'
+import { GrMail } from 'react-icons/gr'
+
 
 function Footer() {
     return (
-        <Box mt='50px' height='150px' backgroundColor='blackAlpha.50'>Footer</Box>
+        <Box mt='50px' height='130px' backgroundColor='blackAlpha.50'>
+            <Flex p='15px' direction='row' height='100%'>
+                <Flex direction='column'  >
+                    <Text mb='10px' color='gray.500' size='sm'>Kontakt</Text>
+                    <HStack ml='15px'>
+                        <BsFillTelephoneFill />
+                        <Text>+420 728 987 953</Text>
+                    </HStack>
+                    <HStack ml='15px'>
+                        <GrMail />
+                        <Text>spajzbojzz@gmail.com</Text>
+                    </HStack>
+                </Flex>
+                <Spacer />
+                <Flex direction='column' >
+                    <Text mb='10px' color='gray.500' size='sm'>Sledujte nás</Text>
+                    <HStack ml='20px'>
+                        <IconButton aria-label='fb-link' onClick={() => window.open("https://www.facebook.com/spajzbojz", '_blank')} icon={<BsFacebook size='30px' />} />
+                        <IconButton aria-label='ig-link' onClick={() => window.open("https://instagram.com/spajzbojz", '_blank')} icon={<BsInstagram size='30px' />} />
+                    </HStack>
+                </Flex>
+            </Flex>
+
+        </Box>
     )
 }
 
