2e7aae079f3cc1ed15e2f088b66904ad778ad552
Martin Hofbauer <m.hofbauer@email.cz>
2023-07-26 18:33:42 +0200

    social icons color on hover + image gallery on home page

--- src/features/home/components/HomePage.tsx
+++ src/features/home/components/HomePage.tsx
@@ -1,18 +1,41 @@
 import { Heading, Text } from '@chakra-ui/react'
+import ImageGallery from "react-image-gallery";
+import concertPhoto1 from "../assets/melodka_1.jpg"
+import concertPhoto2 from "../assets/melodka_2.jpg"
+import concertPhoto3 from "../assets/melodka_3.jpg"
 
+import "react-image-gallery/styles/css/image-gallery.css"
+
+const images = [
+    {
+        original: concertPhoto2,
+        thumbnail: concertPhoto2,
+    },
+    {
+        original: concertPhoto1,
+        thumbnail: concertPhoto1,
+    },
+    {
+        original: concertPhoto3,
+        thumbnail: concertPhoto3,
+    }
+];
 
 function HomePage() {
     return (
         <>
             <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate fugiat consectetur vel mollitia perferendis quos corporis magnam itaque esse, ratione sit, tempora reiciendis optio expedita possimus neque est, ut enim.</Text>
 
-            <Heading mt='30px' size='lg'>Fotky z koncertů</Heading>
+            <Heading mb='15px' mt='30px' size='lg'>Aktuality</Heading>
 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, iure quia! Iste qui eaque provident officiis quae impedit dolore praesentium accusamus, odio officia eum quidem, earum, deleniti excepturi a. Molestias nam ipsum tempore veritatis tenetur obcaecati perspiciatis alias, neque suscipit delectus quis! Iure eos quidem impedit delectus quis accusamus, itaque temporibus omnis repellat nam, est in aperiam quaerat fugiat saepe facilis amet! Repudiandae vitae officia culpa veritatis nulla ullam magnam iusto, quam, quaerat fugit laudantium non voluptas blanditiis distinctio deleniti quos eius beatae? Adipisci, quis atque, cupiditate ut eligendi aliquam rem nostrum quibusdam explicabo esse asperiores distinctio ullam nisi ipsum!
+            <Text>
 
-            <Heading mt='30px' size='lg'>Napsali o nás</Heading>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis amet cupiditate rerum, nostrum est suscipit. Autem, cupiditate! Necessitatibus perspiciatis odio animi, odit corrupti impedit eaque! At architecto rem reiciendis, tempore iste doloremque qui voluptas magni recusandae earum harum ad mollitia tenetur, amet nostrum quas ut odit? Quae illo aliquam suscipit dicta nemo, reiciendis voluptas amet porro quidem perferendis itaque corrupti debitis adipisci, fugiat deserunt voluptate sequi aperiam? Doloribus id ut error placeat doloremque. Dolores vel eligendi nesciunt fugit corrupti repellendus dolor exercitationem facere, dolorum dolorem ad velit, officiis sequi repellat beatae sit, possimus asperiores quibusdam in minima modi consectetur quod.
+            </Text>
 
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis amet cupiditate rerum, nostrum est suscipit. Autem, cupiditate! Necessitatibus perspiciatis odio animi, odit corrupti impedit eaque! At architecto rem reiciendis, tempore iste doloremque qui voluptas magni recusandae earum harum ad mollitia tenetur, amet nostrum quas ut odit? Quae illo aliquam suscipit dicta nemo, reiciendis voluptas amet porro quidem perferendis itaque corrupti debitis adipisci, fugiat deserunt voluptate sequi aperiam? Doloribus id ut error placeat doloremque. Dolores vel eligendi nesciunt fugit corrupti repellendus dolor exercitationem facere, dolorum dolorem ad velit, officiis sequi repellat beatae sit, possimus asperiores quibusdam in minima modi consectetur quod.
+            <Heading mb='30px' mt='30px' size='lg'>Výběr fotek z koncertů</Heading>
+
+            <ImageGallery items={images} />;
 
         </>
     )
